3ac7b04098b48ab110ba3b0bf64870b0356854d9
regvedpande <regregd@outlook.com>
2023-10-31 22:53:03 +0530

    pcm.c

---
+++ My C Programmes/pcm.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int physics, chemistry, math;
+    float average;
+
+    printf("Enter Physics marks: ");
+    scanf("%d", &physics);
+
+    printf("Enter Chemistry marks: ");
+    scanf("%d", &chemistry);
+
+    printf("Enter Mathematics marks: ");
+    scanf("%d", &math);
+
+    average = (physics + chemistry + math) / 3.0; 
+
+    if (physics >= 50 && chemistry >= 50 && math >= 50 && average >= 60) {
+        printf("Pass\n");
+    } else {
+        printf("Fail\n");
+    }
+
+    return 0;
+}
