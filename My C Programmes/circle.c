4dcd5e9972e3d5e26d1dc576d2fc2408fbc72cef
regvedpande <regregd@outlook.com>
2023-10-31 22:20:54 +0530

    circle.c

---
+++ My C Programmes/circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float radius, area;
+    printf("Area of Circle is: ");
+    scanf("%f",&radius);
+    area = 3.14 * radius * radius;
+    printf("Area of Circle is : %f ", area);
+    return 0;
+
+}
