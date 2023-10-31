468cf4c2c1c05938b67627892df0e67d1ae40982
regvedpande <regregd@outlook.com>
2023-10-31 22:31:24 +0530

    subject.c

---
+++ My C Programmes/subject.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int physics, chemistry, biology, total;
+    printf("Enter the value of 3 subjects: ");
+    scanf("%d, %d ,%d", &physics, &chemistry, &biology);
+    total : physics + chemistry + biology;
+    printf ("Total of 3 subjects is:  %d\n", total );
+    return 0;
+
+}
