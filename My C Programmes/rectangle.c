5a2ab76cde84033e3c44b7e6aa56d3355b02a41c
regvedpande <regregd@outlook.com>
2023-10-31 22:10:42 +0530

    rectangle.c

---
+++ My C Programmes/rectangle.c
@@ -0,0 +1,13 @@
+//Area of Rectangle
+#include <stdio.h>
+
+int main() {
+    int l, b, area;
+    printf("Enter the value of l and b: ");
+    scanf("%d %d", &l, &b); 
+
+    area = l * b;
+
+    printf("Area of Rectangle is : %d", area);
+    return 0;
+}
