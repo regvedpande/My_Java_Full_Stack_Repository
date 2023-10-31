0eed741af52e430c7c5f8b913a81eb11b72b3a3e
regvedpande <regregd@outlook.com>
2023-10-31 22:03:33 +0530

    printingintfoatchar.c

---
+++ My C Programmes/printingintfoatchar.c
@@ -0,0 +1,13 @@
+// Printing Integer, Float and Character
+#include<stdio.h>
+int main()
+{
+    int num = 20;
+    float floatnum = 23.20;
+    char ch = 'R';
+    
+    printf("Integer : %d\n",num);
+    printf("Float : %f\n", floatnum);
+    printf("Character : %c\n", ch);
+    return 0;
+} 
