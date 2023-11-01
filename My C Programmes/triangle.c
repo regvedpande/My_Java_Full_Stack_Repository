c42e7a500aaf25b803fd7f8e7c62ab0971876847
regvedpande <regregd@outlook.com>
2023-11-01 15:39:36 +0530

    triangle.c

---
+++ My C Programmes/triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+    int angle1,angle2,angle3, sum;
+    printf("Enter the 3 sides: ");
+    scanf("%d %d %d", &angle1, &angle2, &angle3);
+    sum = angle1 + angle2 + angle3;
+
+    if (sum < 180) {
+    printf("It forms a triangle");} else 
+    {
+    printf("It doesn't form a triangle");
+    }
+
+  return 0;
+}
