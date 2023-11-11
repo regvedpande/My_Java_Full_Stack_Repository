ada094506178f67fbadf33b208a5f578966befb4
regvedpande <regregd@outlook.com>
2023-11-11 18:07:03 +0530

    userinput.c

---
+++ My C Programmes/userinput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int myNum;
+
+  printf("Type a number and press enter: \n"); 
+
+  scanf("%d", &myNum);
+
+  printf("Your number is: %d", myNum);
+
+  return 0;
+}
