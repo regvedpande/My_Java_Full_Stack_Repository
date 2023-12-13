7ba1d719102185f6b33d83a4b25bde3d71909379
regvedpande <149050058+regvedpande@users.noreply.github.com>
2023-12-13 10:40:59 +0530

    Refactored Switch Statement

--- Mkpits_Core_Java/controlflowstatements/SwitchStatement.java
+++ Mkpits_Core_Java/controlflowstatements/SwitchStatement.java
@@ -1,34 +1,47 @@
 package com.mkpits.controlflowstatements;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class SwitchStatement {
 
-	public static void main(String []args) {
+	public static void main(String []args) throws NumberFormatException, IOException {
 		
+		SwitchStatement out = new SwitchStatement();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the Number");
+		int choice =Integer.parseInt(br.readLine());
+		
+		
 		
 		
 		switch (choice) {
 		case 1:
-		
+			System.out.println("Name");
+		     out.createName();
 			break;
 			
 		case 2:
+			System.out.println("Number");
+			 out.createNumber();
 
 			break;
 			
 		case 3:
-
+			System.out.println("Age");
+			 out.createAge();
 			break;
 			
 		case 4:
+			System.out.println("ID");
+			 out.createId();
 
 			break;
 			
 		case 5:
+			System.out.println("Star Sign");
+			 out.createStarSign();
 		
 			break;
 
@@ -36,6 +49,38 @@ public class SwitchStatement {
 			
 			break;
 		}
+		
+	}
+	public void createName() {
+		
+		System.out.println("Regved Pande");
+		
+		
+	}
+	
+	public void createNumber() {
+	
+		System.out.println("9049874883");
+		
+	}
+	
+	public void createAge() {
+		
+		System.out.println("22");
+		
+		
+	}
+	
+	public void createId() {
+		
+		System.out.println("11902027");
+		
+		
+	}
+	
+	public void createStarSign() {
+		
+		System.out.println("Leo");
 		
 	}
 }
