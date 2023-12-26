e5680053ba005b62cac72e501360c1c4f6d44556
regvedpande <149050058+regvedpande@users.noreply.github.com>
2023-12-26 11:30:51 +0530

    Multiple Inheritance

---
+++ Mkpits_Core_Java/multipleinheritance/multipleinheritance.java
@@ -0,0 +1,28 @@
+package com.mkpits.multipleinheritance;
+
+class Apple {
+    void smartphone() {
+        System.out.println("Apple makes the best Smartphones");
+    }
+}
+
+class iPhone extends Apple {
+    void about() {
+        System.out.println("Apple is the maker of iPhones");
+    }
+}
+
+class features extends iPhone {
+    void technicalspecification() {
+        System.out.println("It has a faster processor and best cameras");
+    }
+}
+
+public class multipleinheritance { 
+    public static void main(String[] args) {
+        features f = new features(); 
+        f.smartphone();
+        f.about();
+        f.technicalspecification();
+    }
+}
