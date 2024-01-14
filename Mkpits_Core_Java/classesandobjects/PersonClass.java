a72e1ef045513b556aa4a9068785c5d3771bcf6e
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-01-14 12:43:41 +0530

    Adding Some changes to PersonClass

--- Mkpits_Core_Java/classesandobjects/PersonClass.java
+++ Mkpits_Core_Java/classesandobjects/PersonClass.java
@@ -1,5 +1,3 @@
-package com.mkpits.classesandobjects;
-
 
 // Main class definition
 public class PersonClass {
@@ -14,12 +12,17 @@ public class PersonClass {
         p1.weight = 100;
         p1.height = 5.11f;
 
+        // Calling the static method zodiacsign() for Person1
+        Person1.zodiacsign();
+
         // Printing information about Person1
         System.out.println("Person 1:");
         System.out.println("Name: " + p1.name);
         System.out.println("Age: " + p1.age);
         System.out.println("Weight: " + p1.weight);
         System.out.println("Height: " + p1.height);
+        // Printing the zodiac sign for Person1
+        System.out.println("Zodiac Sign is: " + Person1.zodiacsign());
 
         // Creating an instance of Person2
         Person2 p2 = new Person2();
@@ -28,12 +31,17 @@ public class PersonClass {
         p2.weight = 90;
         p2.height = 6.1f;
 
+        // Calling the static method zodiacsign() for Person2
+        Person2.zodiacsign();
+
         // Printing information about Person2
         System.out.println("\nPerson 2:");
         System.out.println("Name: " + p2.name);
         System.out.println("Age: " + p2.age);
         System.out.println("Weight: " + p2.weight);
         System.out.println("Height: " + p2.height);
+        // Printing the zodiac sign for Person2
+        System.out.println("Zodiac Sign is: " + Person2.zodiacsign());
     }
 
     // Definition of the Person1 class
@@ -42,6 +50,14 @@ public class PersonClass {
         int age;
         int weight;
         float height;
+
+        // Static variable representing the zodiac sign for Person1
+        static String zodiacsign = "Cancer";
+
+        // Static method to print the zodiac sign for Person1
+        static void zodiacsign() {
+            System.out.println("Zodiac Sign for Person1: " + zodiacsign);
+        }
     }
 
     // Definition of the Person2 class
@@ -50,5 +66,13 @@ public class PersonClass {
         int age;
         int weight;
         float height;
+
+        // Static variable representing the zodiac sign for Person2
+        static String zodiacsign = "Sagittarius";
+
+        // Static method to print the zodiac sign for Person2
+        static void zodiacsign() {
+            System.out.println("Zodiac Sign for Person2: " + zodiacsign);
+        }
     }
 }
