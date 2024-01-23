47bb7f5cf56894d2bce4a059fb576a92ff9fca4e
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-01-23 12:00:11 +0530

    ArrayList Important Concepts

---
+++ Mkpits_Core_Java/arraylist/Employee.java
@@ -0,0 +1,55 @@
+package com.mkpits.arraylist;
+
+import java.util.Objects;
+
+public class Employee {
+
+	private String name;
+	private String age;
+
+	public Employee(String name, String age) {
+
+		this.name = name;
+		this.age = age;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(age, other.age) && Objects.equals(name, other.name);
+	}
+
+}
---
+++ Mkpits_Core_Java/arraylist/ImplementingArrayListImportantConcepts.java
@@ -0,0 +1,31 @@
+package com.mkpits.arraylist;
+
+import java.util.ArrayList;
+
+public class ImplementingArrayListImportantConcepts {
+
+	public static void main(String[] args) {
+
+		ArrayList<Employee> get = new ArrayList<Employee>();
+		get.add(new Employee("Rakesh", "30"));
+		get.add(new Employee("Marques", "28"));
+		get.add(new Employee("John", "24"));
+		get.add(new Employee("Narendra", "26"));
+
+		ArrayList<Employee> getanother = new ArrayList<Employee>();
+		getanother.add(new Employee("Rahul", "32"));
+		getanother.add(new Employee("Robert", "29"));
+		getanother.add(new Employee("Natasha", "27"));
+		getanother.add(new Employee("Chris", "21"));
+
+		get.addAll(getanother);
+		for (Employee employee : get) {
+			System.out.println(employee);
+		}
+
+		System.out.println(get);
+		System.out.println(getanother);
+
+	}
+
+}
