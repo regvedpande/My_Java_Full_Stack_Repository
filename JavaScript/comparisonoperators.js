b34f15146e9451541a156262381803000456a554
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-02-25 23:12:52 +0530

    S

---
+++ JavaScript/comparisonoperators.js
@@ -0,0 +1,13 @@
+let a = 9;
+let b = 8;
+console.log(a == b);
+console.log(a != b);
+
+// b = "9"; can say false if compare a==b as their types are different 
+// a === b (Strict Equality): Checks if 'a' and 'b' have both the same value and the same data type.
+console.log(a === b);
+
+// a !== b (Strict Inequality): Checks if 'a' and 'b' have different values or different data types.
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
