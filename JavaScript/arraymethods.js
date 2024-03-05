17fc0c5d3f9f0dae363e8eca1f46362a41cf7e56
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-03-05 22:54:54 +0530

    JavaScript Array Practice

---
+++ JavaScript/arraymethods.js
@@ -0,0 +1,19 @@
+let num = [1, 2, 3, 4, 5];
+
+// Print the data type of the 'num' array using the typeof operator.
+console.log(typeof num);
+
+// Print the length of the 'num' array using the length property.
+console.log(num.length);
+
+// Print the string representation of the 'num' array.
+console.log(num.toString());
+
+// Attempt to access the element at index 2 of the 'num' array using the at method.
+console.log(num.at(2));
+
+// The pop() method removes the last element from an array:
+console.log(num.pop())
+
+// The push() method adds a new element to an array (at the end):
+console.log(num.push("8"))
---
+++ JavaScript/arrays.js
@@ -0,0 +1,20 @@
+
+let list = [2, 5, 8, 9, "Regved", "Apple"];
+
+// Print the values at indexes in the array
+console.log(list[0]);
+console.log(list[5]);
+console.log(list[6]);
+
+// Print the length of the array
+console.log(list.length);
+
+// Update the value at index 5 in the array to "Samsung"
+list[5] = "Samsung";
+console.log(list);
+
+// Add a new element at index 6 in the array with the value "Courage"
+list[6] = "Courage";
+
+// Print the data type of the element at index 6 in the array
+console.log(typeof list[6]);
