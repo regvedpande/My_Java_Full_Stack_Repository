73d744a11dbd5d820953840c014196cca66ce40b
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-04-30 23:50:46 +0530

    Some Brushup

---
+++ JavaScript/fact.js
@@ -0,0 +1,12 @@
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+// Example usage:
+const num = 5;
+const result = factorial(num);
+console.log(`The factorial of ${num} is: ${result}`);
