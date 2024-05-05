a1bbf3c26508da12c4bab3ee36af36fc12c3071d
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-05-05 23:05:17 +0530

    Some Work

---
+++ JavaScript/Cookies/getcookies.js
@@ -0,0 +1,19 @@
+// Function to get a cookie by name
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
+
+// Example usage:
+var username = getCookie("username"); // Get the value of the cookie named "username"
+console.log(username); // Output the value of the cookie to the console
---
+++ JavaScript/Cookies/setcookie.js
@@ -0,0 +1,13 @@
+// Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Example usage:
+setCookie("username", "john_doe", 30); // Set a cookie named "username" with value "john_doe" that expires in 30 days
