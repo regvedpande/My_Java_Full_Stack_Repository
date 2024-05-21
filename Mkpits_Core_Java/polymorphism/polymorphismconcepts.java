d19b45f63e731e0cfe31be693c9df94c90d7d8f1
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-05-21 22:52:24 +0530

    Some Brushup

---
+++ Mkpits_Core_Java/polymorphism/polymorphismconcepts.java
@@ -0,0 +1,29 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class polymorphismconcepts {
+    public static void main(String[] args) {
+        Animal animal1 = new Dog();
+        Animal animal2 = new Cat();
+
+        animal1.makeSound(); // Output: Dog barks
+        animal2.makeSound(); // Output: Cat meows
+    }
+}
