9a79808d683600987ad3d6fdda5b072fc65a1260
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-05-24 16:30:08 +0530

    Some Codes

--- Mkpits_Core_Java/synchronisedlocks/Table.java
+++ Mkpits_Core_Java/synchronisedlocks/Table.java
@@ -1,7 +1,6 @@
 package com.mkpits.synchronisedlocks;
 
 public class Table {
-    
     synchronized public void printTable(int n){
         synchronized(this){
             for (int i = 0; i <= 10; i++) {
