754eb343fe31eb3009c04173852a3c5b91cd7f03
regvedpande <149050058+regvedpande@users.noreply.github.com>
2024-10-10 11:48:20 +0530

    some practice

---
+++ JavaScript/Event/clickanddoubleclick.js
@@ -0,0 +1,27 @@
+// let btn = document.querySelector("#leave");
+// btn.onclick = () => {
+//     console.log("mouse leave");
+// }
+
+// let btn2 = document.addEventListener("click", ()=>{
+//     console.log("Hello Regved");
+// })
+
+
+// let handler =()=>{
+//     console.log("Hello Regved");
+// }
+
+// let removed = document.removeEventListener("click", handler);
+let store = document.querySelector("#mode")
+let background = "light";
+
+let btn = document.addEventListener("click", () =>{
+if(background == "light"){
+    background = "dark";
+    document.querySelector("body").style.backgroundColor = "grey";
+}else{
+    background="light";
+    document.querySelector("body").style.backgroundColor = "white";
+}
+});
