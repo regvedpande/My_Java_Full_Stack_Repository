cff973b4e6f53ebcf4306c5e3a31d473b2f7c93f
Regved Pande <149050058+regvedpande@users.noreply.github.com>
2025-04-18 10:49:52 +0530

    Create basic.js

---
+++ JavaScript/Some Random JavaScript Codes/basic.js
@@ -0,0 +1,5 @@
+// Declare a variable named 'message' and assign it a string value
+let message = "Hello, JavaScript basics!";
+
+// Print the value of the 'message' variable to the console
+console.log(message);
